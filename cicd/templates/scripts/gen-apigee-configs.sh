f92f379c6c674db594c4852b67ab1e616e5af4de
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2022-09-14 16:16:26 +0200

    wip: gen-apigee-config script

--- cicd/templates/scripts/gen-apigee-configs.sh
+++ cicd/templates/scripts/gen-apigee-configs.sh
@@ -2,7 +2,9 @@
 
 main() {
     echo CONFIG_LIST: 
-    echo "${CONFIG_LIST}"
+    for config in '${{ parameters.configList }}'; do 
+        echo "${config}"
+    done
 }
 
 [[ "${0}" == "${BASH_SOURCE[0]}" ]] && main "${*}"
