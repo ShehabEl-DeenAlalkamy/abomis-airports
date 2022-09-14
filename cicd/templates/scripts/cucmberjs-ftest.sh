b4525ad70351ecbb0520aecf22a01c83f727b823
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2022-09-14 23:45:56 +0200

    feat: report CucumberJS in Junit format

--- cicd/templates/scripts/cucmberjs-ftest.sh
+++ cicd/templates/scripts/cucmberjs-ftest.sh
@@ -4,11 +4,15 @@ main() {
     readonly ADO_DEBUG_CMD="##[debug]"
     echo "${ADO_DEBUG_CMD}"creating "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/functional-testing/cucumber
     mkdir -p "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/functional-testing/cucumber
-    echo "TEST_HOST=${HOSTNAME} node node_modules/.bin/cucumber-js --publish-quiet ${SOURCE_PATH} --format json:${BUILD_ARTIFACTSTAGINGDIRECTORY}/functional-testing/cucumber/reports.json"
-    TEST_HOST="${HOSTNAME}" node node_modules/.bin/cucumber-js --publish-quiet "${SOURCE_PATH}" --format json:"${BUILD_ARTIFACTSTAGINGDIRECTORY}"/functional-testing/cucumber/reports.json
-    
-    # avoid flow breaking in case unit testing raises error
-    exit 0
+    echo "TEST_HOST=${HOSTNAME} node node_modules/.bin/cucumber-js --publish-quiet ${SOURCE_PATH} --format json:${BUILD_ARTIFACTSTAGINGDIRECTORY}/functional-testing/cucumber/results.json"
+    if [[ $(TEST_HOST="${HOSTNAME}" node node_modules/.bin/cucumber-js --publish-quiet "${SOURCE_PATH}" --format json:"${BUILD_ARTIFACTSTAGINGDIRECTORY}"/functional-testing/cucumber/results.json) ]]; then
+        ./node_modules/.bin/cucumber-junit <"${BUILD_ARTIFACTSTAGINGDIRECTORY}"/functional-testing/cucumber/results.json >"${BUILD_ARTIFACTSTAGINGDIRECTORY}"/functional-testing/cucumber/results.xml
+        # avoid flow breaking in case unit testing raises error
+        exit 0
+    else
+        # avoid flow breaking in case unit testing raises error
+        exit 0
+    fi
 }
 
 [[ "${0}" == "${BASH_SOURCE[0]}" ]] && main "${*}"
