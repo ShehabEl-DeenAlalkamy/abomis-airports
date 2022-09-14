7b9ae8350cbfbb4e7efc2b05e9fbd2d95ca404b5
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2022-09-15 00:07:17 +0200

    Update cucmberjs-ftest.sh script

--- cicd/templates/scripts/cucmberjs-ftest.sh
+++ cicd/templates/scripts/cucmberjs-ftest.sh
@@ -6,7 +6,7 @@ main() {
     mkdir -p "${BUILD_ARTIFACTSTAGINGDIRECTORY}"/functional-testing/cucumber
     echo "TEST_HOST=${HOSTNAME} node node_modules/.bin/cucumber-js --publish-quiet ${SOURCE_PATH} --format json:${BUILD_ARTIFACTSTAGINGDIRECTORY}/functional-testing/cucumber/results.json"
     TEST_HOST="${HOSTNAME}" node node_modules/.bin/cucumber-js --publish-quiet "${SOURCE_PATH}" --format json:"${BUILD_ARTIFACTSTAGINGDIRECTORY}"/functional-testing/cucumber/results.json
-    [[ "${?}" -ne 0 ]] || exit 0
+    [[ "${?}" -ne 0 ]] && exit 0
     ./node_modules/.bin/cucumber-junit <"${BUILD_ARTIFACTSTAGINGDIRECTORY}"/functional-testing/cucumber/results.json >"${BUILD_ARTIFACTSTAGINGDIRECTORY}"/functional-testing/cucumber/results.xml
     # avoid flow breaking in case unit testing raises error
     exit 0
