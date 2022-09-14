dd32cf351c5e93c87addb786cee2af33c7eb688b
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2022-09-14 17:23:26 +0200

    feat: Generate Apigee config plugin goals dynamically script

--- cicd/templates/scripts/gen-apigee-configs.sh
+++ cicd/templates/scripts/gen-apigee-configs.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 main() {
+    readonly ADO_DEBUG_CMD="##[debug]"
+    echo "${ADO_DEBUG_CMD}"Supplied Configs:
+    echo "${ADO_DEBUG_CMD}"-----------------
     apigee_config_goals
     for config in $(echo "${CONFIG_LIST}" | jq -r '.[]'); do
+        echo "${ADO_DEBUG_CMD}- ${config}":
         apigee_config_goals+=" apigee-config:${config}"
     done
-    echo untrimmed apigee_config_goals: 
-    echo "${apigee_config_goals}"
-    echo trimmed apigee_config_goals: 
-    echo "${apigee_config_goals}" | xargs
+    # trim apigee_config_goals
+    apigee_config_goals=$(echo "${apigee_config_goals}" | xargs)
+    echo "${ADO_DEBUG_CMD}"Apigee Config Goals: "${apigee_config_goals}"
+    echo "##vso[task.setvariable variable=Framework.Config.Mvn.ApigeeConfigGoals]${apigee_config_goals}"
 }
 
 [[ "${0}" == "${BASH_SOURCE[0]}" ]] && main "${*}"
